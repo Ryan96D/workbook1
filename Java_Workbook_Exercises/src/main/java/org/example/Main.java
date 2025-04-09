9efaf178e5d1ebfadcaf1877c9f42f757b6e488d
Ryan <doryan6996@gmail.com>
2025-04-09 16:11:48 -0700

    Workbook exercises (Java) file structure

---
+++ Java_Workbook_Exercises/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        long VHnr = 9999991;
+        String VHmdl = "Dodge Charger";
+        String VHcolor = "Red";
+        boolean towpackage = false;
+        boolean odometerRdng = true;
+        long price = 40000;
+        char qualityrating - 'B';
+        String phone = "(123)123-1233";
+        String SSN = "123-12-1233";
+        long zip = 12345;
+
